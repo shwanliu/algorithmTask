3c049fe6cf6ff7e2503c9755210e7b809ec4f53c
shwanliu <614477862@qq.com>
2019-08-12 15:26:42 +0800

    update task3

--- task3/mysort.py
+++ task3/mysort.py
@@ -24,7 +24,7 @@ def mergeSorted(nums):
 # 2、分区过程，将比这个数大的数全放到它的右边，小于或等于它的数全放到它的左边
 # 3、再对左右区间重复第二步，直到各区间只有一个数
 
-#version1.直接取每一个递归list中的第一个元素作为基准元素
+#version1
 def qucikSortedV1(nums):
     if len(nums)<=2:
         return nums
@@ -34,17 +34,75 @@ def qucikSortedV1(nums):
         right = [x for x in nums[1:] if x> pivot]
         return qucikSortedV1(left)+[pivot]+qucikSortedV1(right)
 
-# def qucikSortedV2
-# #插入排序
+#不以第一个为基准，而是找到一个合适的基准
+def qucikSortedV2(nums,left,right):
+    if left < right:
+        pivot = Partition(nums,left,right)
+        qucikSortedV2(nums,left,pivot-1)
+        qucikSortedV2(nums,pivot+1,right)
+    return nums
 
+def Partition(nums,left,right):
+    pivotkey = nums[left]
+    # print(pivotkey)
+    while left < right:
+        # 从右向左，如果当前指向的元素大于基准的话，向右继续走
+        while left < right and nums[right] >= pivotkey:
+            right-=1
+        nums[left] = nums[right]
+        # 从左向右，如果当前指向的元素小于基准的话，向左继续走
+        while left < right and nums[left] <= pivotkey:
+            left+=1
+        nums[right] = nums[left]
+
+    nums[left] = pivotkey
+    return left
+
+#插入排序
+def insertSorted(nums):
+    for i in range(1,len(nums)):
+        j = i-1
+        key = nums[i]
+        # 如果当前的nums[j]大于目前需安排的key的位置，则需要nums[j前面找一个插入的位置，所以将nums[j]往后移动
+        # 当key>nums[j]的时候，则为key插入的位置
+        while key <nums[j] and j>=0 :
+            nums[j+1]=nums[j]
+            j-=1
+        nums[j+1] = key            
+    return nums
+            
 #冒泡排序
+def bubbleSorted(nums):
+    
+    while True:
+        c = 0
+        for i in range(1,len(nums)):
+            if nums[i-1]>nums[i]:
+                nums[i-1],nums[i] =nums[i], nums[i-1],
+                c+=1
+        if c == 0:
+            break
+    return nums
 
 
 #选择排序
+def selectionSorted(nums):
+    res = []
+    while len(nums)!=0:
+        res.append(min(nums))
+        nums.remove(min(nums))
+    return res
+
+
+
 
 
 #堆排序
 
 if __name__ == "__main__":
-    print(mergeSorted([7,5,1,6,8,3,13]))
-    print(qucikSortedV1([7,5,1,6,8,3,13]))
+    # print(mergeSorted([7,5,1,6,8,3,13]))
+    # print(qucikSortedV1([7,5,1,6,8,3,13]))
+    # print(qucikSortedV2([7,5,1,6,8,3,13],0,6))
+    # print(insertSorted([7,5,1,6,8,3,13]))
+    # print(bubbleSorted([7,5,1,6,8,3,13]))
+    print(selectionSorted([7,5,1,6,8,3,13]))
--- task3/task3.md
+++ task3/task3.md
@@ -1,6 +1,7 @@
 ## Task. 3
 ### 排序
-* 实现归并排序、快速排序、插入排序、冒泡排序、选择排序、堆排序（完成leetcode上的返回滑动窗口中的最大值(239)，这是上一期第三天的任务进行保留（涉及队列可以对第二天进行整理复习））
+* 实现归并排序、快速排序、插入排序、冒泡排序、选择排序、堆排序
+* 完成leetcode上的返回滑动窗口中的最大值(239)，这是上一期第三天的任务进行保留（涉及队列可以对第二天进行整理复习
 * 编程实现 O(n) 时间复杂度内找到一组数据的第 K 大元素
 
 ### 二分查找
@@ -12,7 +13,7 @@
 * Sliding Window Maximum（滑动窗口最大值） \
     英文版：https://leetcode.com/problems/sliding-window-maximum/
     中文版：https://leetcode-cn.com/problems/sliding-window-maximum/
-    
+
 * Sqrt(x) （x 的平方根）\
 英文版：https://leetcode.com/problems/sqrtx/
 中文版：https://leetcode-cn.com/problems/sqrtx/
